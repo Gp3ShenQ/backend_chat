152268868a72f7313953739615c4eaf2e3e1d8f0
Amos <gp3g4xup6@gmail.com>
2025-02-27 01:28:27 +0800

    golbol

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   compatibilityDate: "2024-11-01",
+// });
+
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  nitro: {
+    routeRules: {
+      '/api/**': { cors: true }
+    }
+  }
 });
