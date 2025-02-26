124da44852c5656d9588d2d8fa1559e59516f9f1
Amos <gp3g4xup6@gmail.com>
2025-02-27 01:24:44 +0800

    get test

--- server/api/register/checkRegisterAccountGet.ts
+++ server/api/register/checkRegisterAccountGet.ts
@@ -1,14 +1,19 @@
 import { defineEventHandler } from "h3";
 import connectDB from "~/server/db/mongoose";
-import User from "~/server/models/user_models/User";
+// import User from "~/server/models/user_models/User";
 
 export default defineEventHandler(async (event) => {
-  await connectDB();
+  // await connectDB();
+
+  event.node.res.statusCode = 500; // 設置狀態碼
+  event.node.res.setHeader('Access-Control-Allow-Origin', '*'); // 或指定你的前端域名
+  event.node.res.end(JSON.stringify({ message: "註冊成功" })); // 結束回應
 
   const get = event.node.req.method === "GET";
 
   if (get) {
-    const UserAccount = await User.find({}, { account: 1, _id: 0 });
-    return { ...UserAccount };
+    // const UserAccount = await User.find({}, { account: 1, _id: 0 });
+    // return { ...UserAccount };
+    
   }
 });
